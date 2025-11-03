fe6c27034fbf286b67075b3b2712a131b5b15dc7
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-11-03 10:41:12 +0100

    refactor: remove unnecessary blank line in placeOrder method of OrderController

--- monolith/src/main/java/com/optivem/atddaccelerator/eshop/monolith/api/controller/OrderController.java
+++ monolith/src/main/java/com/optivem/atddaccelerator/eshop/monolith/api/controller/OrderController.java
@@ -23,7 +23,6 @@ public class OrderController {
         var response = orderService.placeOrder(request);
         var location = URI.create("/api/orders/" + response.getOrderNumber());
         return ResponseEntity.created(location).body(response);
-
     }
 
     @GetMapping("/api/orders/{orderNumber}")
