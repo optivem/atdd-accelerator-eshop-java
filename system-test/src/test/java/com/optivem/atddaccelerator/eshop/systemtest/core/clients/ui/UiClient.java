347ab5c8ea86cae286fdf02ec2df18f19b5d2de4
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-11-03 11:24:37 +0100

    feat: update order cancellation method to use POST and improve response handling

--- system-test/src/test/java/com/optivem/atddaccelerator/eshop/systemtest/core/clients/ui/UiClient.java
+++ system-test/src/test/java/com/optivem/atddaccelerator/eshop/systemtest/core/clients/ui/UiClient.java
@@ -22,8 +22,6 @@ public class UiClient implements AutoCloseable {
         this.page = browser.newPage();
 
         this.homePage = new HomePage(page, baseUrl);
-
-        page.onDialog(Dialog::accept); // Auto-accept the alert
     }
 
     public HomePage openHomePage() {
