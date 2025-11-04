2968625c3af9fbd2ae77141e44007b5506bb9986
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-11-04 12:11:24 +0100

    Update dependencies and refactor channel imports

--- system-test/src/test/java/com/optivem/atddaccelerator/eshop/systemtest/smoketests/SmokeTest.java
+++ system-test/src/test/java/com/optivem/atddaccelerator/eshop/systemtest/smoketests/SmokeTest.java
@@ -2,8 +2,8 @@ package com.optivem.atddaccelerator.eshop.systemtest.smoketests;
 
 import com.optivem.atdd.commons.MathCommons;
 import com.optivem.atddaccelerator.eshop.systemtest.TestConfiguration;
-import com.optivem.atddaccelerator.eshop.systemtest.commons.channels.Channel;
-import com.optivem.atddaccelerator.eshop.systemtest.commons.channels.ChannelExtension;
+import com.optivem.atdd.commons.channels.Channel;
+import com.optivem.atdd.commons.channels.ChannelExtension;
 import com.optivem.atddaccelerator.eshop.systemtest.core.drivers.ChannelType;
 import com.optivem.atddaccelerator.eshop.systemtest.core.drivers.Driver;
 import com.optivem.atddaccelerator.eshop.systemtest.core.drivers.DriverFactory;
